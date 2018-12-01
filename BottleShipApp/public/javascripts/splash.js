77d790079ffcfc97ce6fced6b7b0725fe0a3ca98
Eniko Kollar <eniko.kollar@yahoo.com>
2018-12-01 21:37:02 +0100

    js for the splash screen

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -2,3 +2,12 @@ function sayHello() {
     alert("Hello");
 }
 
+function save(){
+    var text_to_save=document.getElementById('submitButton').value;
+    localStorage.setItem("playersName", text_to_save); // save the item
+    }
+
+function retrieve(){
+    var playersName=localStorage.getItem("playersName"); // retrieve
+    document.getElementById('textDiv').innerHTML = playersName; // display
+}
