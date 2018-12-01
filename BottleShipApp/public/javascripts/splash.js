d02cc475f62718df097d17ddffb696c0ac405c1a
Csanád <bakoscsanad@gmail.com>
2018-12-01 22:30:55 +0100

    splash updated

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -1,43 +1,51 @@
-//don't use sloppy parts of JS
-"use strict";
-
-//event handler for clicking on the button
-//listens on the button
-    $(".username_input button").on("click", function(event) {
-      addNickNameFromInput();
-    });
-
-    var addNickNameFromInput = function () {
-
-        //declare the variable to store the new username
-        var new_user = "";
-        sayHello(new_user);
+var main = function() {
     
-        if($(".username_input input").val() !== "") {
+    
+    //don't use sloppy parts of JS
+    "use strict";
 
-            //then get the entered value from the input field
-            new_user = $("<p>").text($(".username_input input").val());
+    //event handler for clicking on the button
+    //listens on the button
+$(".username_input button").on("click", function(event) {
+    sayHello();
+    savePlayerName();
+    retrievePlayerName();
+});
 
-            //clear the input field
-            $(".comment-input input").val("");
-            sayHello();
+//if user entered her/his name then start the game
+var addNickNameFromInput = function () {
 
-        }
+    //declare the variable to store the new username
+    var new_user = "";
+    sayHello();
+    
+    if($(".username_input input").val() !== "") {
+
+        //then get the entered value from the input field
+        new_user = $("<p>").text($(".username_input input").val());
+
+        //clear the input field
+        $(".comment-input input").val("");
+        sayHello();
+
     }
+}
 
-    function sayHello(username) {
-         alert("Hello " + username);
-    }
+function sayHello() {
+    alert("Hello ");
+}
 
 
-function save(){
+function savePlayerName(){
     var text_to_save=document.getElementById('submitButton').value;
-    localStorage.setItem("playersName", text_to_save); // save the item
+    localStorage.setItem("playerName", text_to_save); // save the item
     }
 
-function retrieve(){
+function retrievePlayerName(){
     var playersName=localStorage.getItem("playersName"); // retrieve
     document.getElementById('textDiv').innerHTML = playersName; // display
+}
+
 }
 
 $(document).ready(main);
