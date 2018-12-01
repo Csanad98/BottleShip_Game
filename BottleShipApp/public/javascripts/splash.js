c9692165204625295b43a8a4772bad03d953c068
Csanád <bakoscsanad@gmail.com>
2018-12-01 21:44:42 +0100

    splash updated

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -1,5 +1,36 @@
-function sayHello() {
-    alert("Hello");
+var main = function() {
+    "use strict";
+
+    //event handler for clicking on the button
+    //listens on the button
+    $(".username_input button").on("click", function(event) {
+      addNickNameFromInput();
+    });
+
+    var addNickNameFromInput = function () {
+
+        //declare the variable to store the new username
+        var new_user = "";
+        sayHello(new_user);
+    
+        if($(".username_input input").val() !== "") {
+
+            //then get the entered value from the input field
+            new_user = $("<p>").text($(".username_input input").val());
+
+            //clear the input field
+            $(".comment-input input").val("");
+            sayHello();
+
+        }
+    }
+
+    function sayHello(username) {
+         alert("Hello " + username);
+    }
+
+
+
 }
 
 function save(){
@@ -11,3 +42,5 @@ function retrieve(){
     var playersName=localStorage.getItem("playersName"); // retrieve
     document.getElementById('textDiv').innerHTML = playersName; // display
 }
+
+$(document).ready(main);
