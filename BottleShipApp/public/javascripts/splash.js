5bf54311df7de9c5dc8919a841b521ee9c7f0f7d
Csanád <bakoscsanad@gmail.com>
2018-12-01 23:36:28 +0100

    save username

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -1,51 +1,18 @@
-var main = function() {
-    
-    
-    //don't use sloppy parts of JS
-    "use strict";
-
-    //event handler for clicking on the button
-    //listens on the button
-$(".username_input button").on("click", function(event) {
-    sayHello();
-    savePlayerName();
-    retrievePlayerName();
-});
-
-//if user entered her/his name then start the game
-var addNickNameFromInput = function () {
-
-    //declare the variable to store the new username
-    var new_user = "";
-    sayHello();
-    
-    if($(".username_input input").val() !== "") {
-
-        //then get the entered value from the input field
-        new_user = $("<p>").text($(".username_input input").val());
-
-        //clear the input field
-        $(".comment-input input").val("");
-        sayHello();
-
-    }
-}
-
-function sayHello() {
-    alert("Hello ");
-}
 
 
-function savePlayerName(){
-    var text_to_save=document.getElementById('submitButton').value;
+
+var savePlayerName = function(){
+    var text_to_save=document.getElementById('playerName').value;
+    alert(text_to_save);
     localStorage.setItem("playerName", text_to_save); // save the item
-    }
+    var user=localStorage.getItem("playerName"); // retrieve
+    retrievePlayerName();
+    alert("Hello " + user );
+}
 
 function retrievePlayerName(){
-    var playersName=localStorage.getItem("playersName"); // retrieve
+    var playersName=localStorage.getItem("playerName"); // retrieve
     document.getElementById('textDiv').innerHTML = playersName; // display
 }
 
-}
 
-$(document).ready(main);
