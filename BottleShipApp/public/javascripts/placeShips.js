2ddfa463e68fdca70e8af52a580dd8e688b43ced
Csanád <bakoscsanad@gmail.com>
2018-12-02 17:09:51 +0100

    2d array of board added

--- BottleShipApp/public/javascripts/placeShips.js
+++ BottleShipApp/public/javascripts/placeShips.js
@@ -41,6 +41,22 @@ for (var i = 0; i<cols; i++) {
 }
 
 
+
+//create 2D array for internal representation of the board
+var boardArray = Array(cols);
+
+for (var i = 0; i<cols; i++) {
+    boardArray[i] = Array(rows);
+}
+
+for(var i = 0; i<cols; i++) {
+    for(var e = 0; e<rows; e++) {
+        boardArray[i][e] = 0;
+    }
+}
+
+
+
 //drag and drop events
 function allowDrop(ev) {
     ev.preventDefault();
@@ -58,9 +74,9 @@ function drop(ev) {
 
 // grid for the board. ship sizes = 1,2,3 no ship = 0
 yourBoard: [[0, 0, 0, 0, 0, 0, 0, 0],     // y = 0
-       [0, 0, 0, 0, 0, 0, 0, 0],     // y = 1
-       [0, 0, 0, 0, 0, 0, 0, 0],     // y = 2
-       [0, 0, 0, 0, 0, 0, 0, 0],     // y = 3
-       [0, 0, 0, 0, 0, 0, 0, 0],     // y = 4
-       [0, 0, 0, 0, 0, 0, 0, 0],     // y = 5
-       [0, 0, 0, 0, 0, 0, 0, 0]]
+            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 1
+            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 2
+            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 3
+            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 4
+            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 5
+            [0, 0, 0, 0, 0, 0, 0, 0]]
