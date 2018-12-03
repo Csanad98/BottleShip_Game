74507d4f90dd29646ea5fde9099c1e5dc1142327
Csanád <bakoscsanad@gmail.com>
2018-12-03 20:17:01 +0100

    grid representation update

--- BottleShipApp/public/javascripts/placeShips.js
+++ BottleShipApp/public/javascripts/placeShips.js
@@ -151,12 +151,3 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
-
-// grid for the board. ship sizes = 1,2,3 no ship = 0
-yourBoard: [[0, 0, 0, 0, 0, 0, 0, 0],     // y = 0
-            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 1
-            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 2
-            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 3
-            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 4
-            [0, 0, 0, 0, 0, 0, 0, 0],     // y = 5
-            [0, 0, 0, 0, 0, 0, 0, 0]]
