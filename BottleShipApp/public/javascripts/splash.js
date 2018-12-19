c0e3bcbd2ece7ded6bf6efdc8bc690bf9ac78a51
Eniko Kollar <eniko.kollar@yahoo.com>
2018-12-20 00:00:02 +0100

    css animation issues resolved

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -5,6 +5,7 @@ var socket = new WebSocket("ws://localhost:3000");
             /* socket.onmessage = function(event){
                 document.getElementById("hello").innerHTML = event.data;
             } */
+            /*
             socket.onopen = function(){
                 socket.send(JSON.stringify({messageType: "connect"}));
                 socket.send(JSON.stringify({messageType: "readyToPlay"}));
