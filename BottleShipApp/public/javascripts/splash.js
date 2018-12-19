e9b1d16dbfa6fb06a4af4b0447954aa7223d2ae1
Eniko Kollar <eniko.kollar@yahoo.com>
2018-12-19 16:34:58 +0100

    css element added: before, after, active, transition, aimation

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -1,25 +1,4 @@
 
-//save user's name, which was entered to the input box
-var savePlayerName = function(){
-    var text_to_save=document.getElementById('playerName').value;
-    
-    //if the input field is not empty
-    if(text_to_save !== "") {
-
-        localStorage.setItem("player_name", text_to_save); // save the item
-        alert("Hello " + retrievePlayerName() + "! Click OK to get started with placing your ships!");
-        window.open("game.html", "_self");
-
-    } else if(text_to_save === "") {
-        alert("Please add your name to the input field to get started.");
-    }
-
-}
-
-//returns the player's name which was saved to the localStorage
-var retrievePlayerName = function (){
-    return localStorage.getItem("player_name"); // retrieve
-}
 
 //using web sockets
 var socket = new WebSocket("ws://localhost:3000");
@@ -31,3 +10,7 @@ var socket = new WebSocket("ws://localhost:3000");
                 socket.send(JSON.stringify({messageType: "readyToPlay"}));
                 document.getElementById("hello").innerHTML = "Sending a first message to the server ...";
             };
+
+// var goToGameScreen = function(){
+//     alert("should go to game screen")
+// }
