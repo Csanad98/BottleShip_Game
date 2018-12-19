e2361e9a93d07c0fe7b4df31d164e020037fea2e
Eniko Kollar <eniko.kollar@yahoo.com>
2018-12-19 16:48:43 +0100

    comments updated

--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -8,9 +8,5 @@ var socket = new WebSocket("ws://localhost:3000");
             socket.onopen = function(){
                 socket.send(JSON.stringify({messageType: "connect"}));
                 socket.send(JSON.stringify({messageType: "readyToPlay"}));
-                document.getElementById("hello").innerHTML = "Sending a first message to the server ...";
             };
 
-// var goToGameScreen = function(){
-//     alert("should go to game screen")
-// }
