b67381a57e4273482ffece6fc8b006d0727b8962
Eniko Kollar <eniko.kollar@yahoo.com>
2018-12-20 11:33:49 +0100

    everything working

--- BottleShipApp/app.js
+++ BottleShipApp/app.js
@@ -5,9 +5,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const config = require('./config/config');
-const Database = require('./database');
-const gameController = require('./gameController'); 
-
 
 let indexRouter = require('./routes/index');
 let playRouter = require('./routes/play');
@@ -27,33 +24,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(config.cookie.secret));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//statistics
-var distinctUsers = Database.getPlayerCount();
-var playersWaiting = gameController.playersReadyToStart.length;
-console.log(playersWaiting);
-var leastMoves = 1;
-
-function updateStatistics(){
-    distinctUsers = Database.getPlayerCount();
-    playersWaiting = gameController.playersReadyToStart.length;
-    leastMoves = 2;
-};
-
-
-app.get('/', (req, res) => {
-    updateStatistics();
-    //console.log(distinctUsers); 
-    res.render('splash.ejs', { distinctUsersPlayed: distinctUsers, waitingRoom: playersWaiting, fastestWinner: leastMoves});
-})
-
-app.get('/splash', (req, res) => {
-    updateStatistics();
-    //console.log(distinctUsers);
-    res.render('splash.ejs', { distinctUsersPlayed: leastMoves, waitingRoom: playersWaiting, fastestWinner: leastMoves});
-})
-
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/play', playRouter);
-//app.use('/splash', splashRouter);
+app.use('/splash', splashRouter);
 
 module.exports = app;
