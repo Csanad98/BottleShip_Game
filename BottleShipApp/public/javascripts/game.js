faf1f890242918c2c95b25340822a09bf27aface
Eniko Kollar <eniko.kollar@yahoo.com>
2018-12-20 01:01:18 +0100

    fullscreen mode added

--- BottleShipApp/public/javascripts/game.js
+++ BottleShipApp/public/javascripts/game.js
@@ -1412,3 +1412,17 @@ function setEnemyBoardTitle() {
     document.getElementById("enemyBoardTitle").innerHTML = "Enemy Board";
 
 };
+
+//function for full screen mode
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  } 
+}   
--- BottleShipApp/public/javascripts/splash.js
+++ BottleShipApp/public/javascripts/splash.js
@@ -1,10 +1,24 @@
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  } 
+}
+
+
 /*
 
 //using web sockets
 var socket = new WebSocket("ws://localhost:3000");
             /* socket.onmessage = function(event){
                 document.getElementById("hello").innerHTML = event.data;
-            } */
+            } */ /*
             socket.onopen = function(){
                 socket.send(JSON.stringify({messageType: "connect"}));
                 socket.send(JSON.stringify({messageType: "readyToPlay"}));
